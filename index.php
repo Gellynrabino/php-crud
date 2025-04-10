d15b7b8e1ef75205f3394d787259cb1cc5624d5f
Gellynrabino <gellynrabino5@gmail.com>
2025-04-10 18:19:54 +0800

    gellyn

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>CRUD APPLICATION </h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>first Name</th>
+                <th>last Name</th>
+                <th>age</th>
+            </tr>         
+        </thead>
+</tbody>
+<tr>
+              <td>3</td>
+              <td>Gellyn</td>
+              <td>Rabino</td>
+              <td>19</td>
+</tr>
+<tr>
+              <td>5</td>
+              <td>Mae</td>
+              <td>Rodriguez</td>
+              <td>21</td>
+        </tr>
+    </tbody>
+    </table>
+</body>
+</html>
